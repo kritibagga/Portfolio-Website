0fa62e7db61757276f9c6673f9efc038e001a62b
kriti bagga <bagga.kriti@gmail.com>
2023-04-05 17:39:07 -0400

    updated and coded about page

--- portfolio/src/Portfolio.jsx
+++ portfolio/src/Portfolio.jsx
@@ -1,19 +1,49 @@
-
 //import  styles from './Portfolio.module.css';
-import React from 'react';
-import HomePage from './HomePage.jsx';
-import NavBar from './NavBar.jsx';
-import Footer from './Footer.jsx';
-
+import React from "react";
+import HomePage from "./HomePage.jsx";
+import NavBar from "./NavBar.jsx";
+import Footer from "./Footer.jsx";
+import AboutMe from "./AboutMe.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function Portfolio() {
-  return (
-    <>
-    <NavBar />
-    <HomePage />
-    <Footer />
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<>
+							<HomePage /> <Footer />
+						</>
+					}
+				/>
+				<Route
+					path='/home'
+					element={
+						<>
+							<HomePage /> <Footer />
+						</>
+					}
+				/>
+
+				<Route
+					path='/home/about'
+					element={<AboutMe />}
+				/>
+				{/* <Route
+					path='/home/contact'
+					element={<Contact />}
+				/>
+				<Route
+					path='/home/project'
+					element={<Project />}
+				/> */}
+			</Routes>
+		</>
+	);
 }
 
 export default Portfolio;
---
+++ portfolio/src/Timeline.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./Timeline.module.css";
+
+export default function Timeline() {
+	return (
+		<div className="animation">
+			<hr className={styles.break} />
+			<h4 className={styles.title}>My Timeline</h4>
+			<div className={styles.timeline}>
+				<div className={styles.timelineItem}>
+					<div className={styles.timelineIcon}>
+						<i className='fas fa-briefcase'></i>
+					</div>
+					<p className={styles.timelineDuration}>2021 - 2023</p>
+					<h5 className={styles.itemHeading}>
+						Front-End Developer<span> - Agency Analytics</span>
+					</h5>
+					<p> Toronto, Canada</p>
+				</div>
+				<div className={styles.timelineItem}>
+					<div className={styles.timelineIcon}>
+						<i className='fas fa-briefcase'></i>
+					</div>
+					<p className={styles.timelineDuration}>2019 - 2021</p>
+					<h5 className={styles.itemHeading}>
+						Electrical & Computer Science (M.Eng)
+						<span> - Ontario Tech University</span>
+					</h5>
+					<p>Oshawa, Canada</p>
+				</div>
+				<div className={styles.timelineItem}>
+					<div className={styles.timelineIcon}>
+						<i className='fas fa-briefcase'></i>
+					</div>
+					<p className={styles.timelineDuration}>2017 - 2019</p>
+					<h5 className={styles.itemHeading}>
+						Programmer Analyst<span> - Cognizant</span>
+					</h5>
+					<p>Pune, India</p>
+				</div>
+				<div className={styles.timelineItem}>
+					<div className={styles.timelineIcon}>
+						<i className='fas fa-briefcase'></i>
+					</div>
+					<p className={styles.timelineDuration}>2012 - 2016</p>
+					<h5 className={styles.itemHeading}>
+						Electrical and Electronics Engineering (B.Tech)<span> - JNTUH</span>
+					</h5>
+					<p>Hyderabad, India</p>
+				</div>
+			</div>
+		</div>
+	);
+}
