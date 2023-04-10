81cc9049bb5db4d89c61f1a2bc88b19469be0ae6
kriti bagga <bagga.kriti@gmail.com>
2023-04-10 18:01:25 -0400

    Contact Page updated

--- portfolio/src/AboutMe.jsx
+++ portfolio/src/AboutMe.jsx
@@ -11,8 +11,7 @@ function AboutMe() {
 					<div className={styles.introPara}>
 						<h1>About Me</h1>
 						<p>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry. Lorem Ipsum has been the industry's standard dummy text
+						A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product. Lorem Ipsum has been the industry's standard dummy text
 							ever since the 1500s, when an unknown printer took a galley of
 							type and scrambled it to make a type specimen book. It has
 							survived not only five centuries, but also the leap into
