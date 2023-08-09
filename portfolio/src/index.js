ce87fce3d9f968adeaf9b2cdb07c5010b8c3130d
kriti bagga <bagga.kriti@gmail.com>
2023-08-09 16:55:21 -0400

    form error solved

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -5,13 +5,16 @@ import Portfolio from "./Portfolio.jsx";
 import { HashRouter } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { StrictMode } from 'react';
 
 AOS.init();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+	<StrictMode>
 	<HashRouter>
 		<Portfolio />
 	</HashRouter>
+	</StrictMode>
 );
